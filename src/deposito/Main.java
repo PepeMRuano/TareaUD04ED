2376f44772a2bcbdc1e73c5521d478bd75330f92
PepeMR <pepe@pmruano.es>
2023-03-02 19:54:29 +0100

    Fichero PPINCIPAL modificado

--- src/deposito/Main.java
+++ src/deposito/Main.java
@@ -11,18 +11,18 @@ public class Main {
 	private static void extracted() {
 		CuentaBanco miCuenta;
 		double saldoActual;
-		miCuenta = new CuentaBanco("Antonio López","1000-2365-85-1230456789",2500,0);
+		miCuenta = new CuentaBanco("Antonio López","1000-2365-85-1230456789",5000,0);
         saldoActual = miCuenta.estado();
         System.out.println("El saldo actual es "+ saldoActual );
 
         try {
-            miCuenta.retirar(2300);
+            miCuenta.retirar(4000);
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
         try {
             System.out.println("Ingreso en cuenta");
-            miCuenta.ingresar(695);
+            miCuenta.ingresar(2000);
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
